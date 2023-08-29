f15a3715a5f11ecf027ec20e75cb9e4087777f3f
vishalx360 <25267239+vishalx360@users.noreply.github.com>
2023-08-30 01:14:43 +0530

    remove unncecessary test

---
+++ __tests__/Sample.test.tsx
@@ -0,0 +1,21 @@
+// import FormPreview from "@/components/FormPreview";
+// import "@testing-library/jest-dom";
+// import { render } from "@testing-library/react";
+
+// describe("Form Preview Component", () => {
+//   it("renders form details correctly", () => {
+//     const { getByAltText, getByText } = render(
+//       <FormPreview form={
+//         {
+//           id: "ABCD",
+//           title: "Test",
+//           description: "Test Description"
+//         }
+//       } />
+//     );
+
+//     const titleElement = getByText("Test");
+//     expect(titleElement).toBeInTheDocument();
+//   });
+// });
+
