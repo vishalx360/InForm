f62bce96f2903b00c76894c50ca1fc401fb85d15
vishalx360 <25267239+vishalx360@users.noreply.github.com>
2023-08-30 18:58:58 +0530

    feat: add DashboardLayout in Signup Page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -25,6 +25,7 @@ import { api } from "@/utils/api";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import DashboardLayout from "@/components/DashboardLayout";
 
 export default function SimpleCard() {
   const toast = useToast();
@@ -48,132 +49,134 @@ export default function SimpleCard() {
     await signIn(provider);
   }
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Head>
-        <title>InForm | Sign up</title>
-      </Head>
-      <Stack spacing={8} mx={"auto"} w="lg" maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Create new account</Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            To create custom forms.
-          </Text>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow={"lg"}
-          p={8}
-        >
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              password: "",
-            }}
-            validationSchema={toFormikValidationSchema(SignUpSchema)}
-            onSubmit={(values) => {
-              mutation.mutate(values);
-            }}
+    <DashboardLayout>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Head>
+          <title>InForm | Sign up</title>
+        </Head>
+        <Stack spacing={8} mx={"auto"} w="lg" maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Create new account</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              To create custom forms.
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
           >
-            <Form className="space-y-4 md:space-y-6">
-              <Stack spacing={4}>
-                <Field name="name">
-                  {({ field, meta }: FieldProps) => (
-                    <FormControl id="name">
-                      <FormLabel>Name</FormLabel>
-                      <Input
-                        type="text"
-                        placeholder="Name"
-                        required
-                        {...field}
-                      />
-                      {meta.touched && meta.error && (
-                        <p className="ml-2 mt-2 text-sm text-red-500">
-                          {meta.error}
-                        </p>
-                      )}
-                    </FormControl>
-                  )}
-                </Field>
-                <Field name="email">
-                  {({ field, meta }: FieldProps) => (
-                    <FormControl id="email">
-                      <FormLabel>Email</FormLabel>
-                      <Input
-                        type="email"
-                        placeholder="name@company.com"
-                        required
-                        {...field}
-                      />
-                      {meta.touched && meta.error && (
-                        <p className="ml-2 mt-2 text-sm text-red-500">
-                          {meta.error}
-                        </p>
-                      )}
-                    </FormControl>
-                  )}
-                </Field>
-                <Field name="password">
-                  {({ field, meta }: FieldProps) => (
-                    <FormControl id="password">
-                      <FormLabel>Password</FormLabel>
-                      <Input
-                        type="password"
-                        placeholder="password"
-                        required
-                        {...field}
-                      />
-                      {meta.touched && meta.error && (
-                        <p className="ml-2 mt-2 text-sm text-red-500">
-                          {meta.error}
-                        </p>
-                      )}
-                    </FormControl>
-                  )}
-                </Field>
-                <Stack spacing={10}>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                password: "",
+              }}
+              validationSchema={toFormikValidationSchema(SignUpSchema)}
+              onSubmit={(values) => {
+                mutation.mutate(values);
+              }}
+            >
+              <Form className="space-y-4 md:space-y-6">
+                <Stack spacing={4}>
+                  <Field name="name">
+                    {({ field, meta }: FieldProps) => (
+                      <FormControl id="name">
+                        <FormLabel>Name</FormLabel>
+                        <Input
+                          type="text"
+                          placeholder="Name"
+                          required
+                          {...field}
+                        />
+                        {meta.touched && meta.error && (
+                          <p className="ml-2 mt-2 text-sm text-red-500">
+                            {meta.error}
+                          </p>
+                        )}
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="email">
+                    {({ field, meta }: FieldProps) => (
+                      <FormControl id="email">
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                          type="email"
+                          placeholder="name@company.com"
+                          required
+                          {...field}
+                        />
+                        {meta.touched && meta.error && (
+                          <p className="ml-2 mt-2 text-sm text-red-500">
+                            {meta.error}
+                          </p>
+                        )}
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {({ field, meta }: FieldProps) => (
+                      <FormControl id="password">
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                          type="password"
+                          placeholder="password"
+                          required
+                          {...field}
+                        />
+                        {meta.touched && meta.error && (
+                          <p className="ml-2 mt-2 text-sm text-red-500">
+                            {meta.error}
+                          </p>
+                        )}
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Stack spacing={10}>
+                    <Button
+                      type="submit"
+                      colorScheme="teal"
+                      leftIcon={<LucideArrowRight />}
+                      isLoading={mutation.isLoading}
+                      loadingText="Signing up..."
+                    >
+                      Sign up
+                    </Button>
+                  </Stack>
                   <Button
-                    type="submit"
-                    colorScheme="teal"
-                    leftIcon={<LucideArrowRight />}
-                    isLoading={mutation.isLoading}
-                    loadingText="Signing up..."
+                    onClick={() => {
+                      void handelOauthSignin("github");
+                    }}
+                    variant="outline"
+                    size="md"
+                    leftIcon={<FaGithub />}
                   >
-                    Sign up
+                    Continue with Github
                   </Button>
-                </Stack>
-                <Button
-                  onClick={() => {
-                    void handelOauthSignin("github");
-                  }}
-                  variant="outline"
-                  size="md"
-                  leftIcon={<FaGithub />}
-                >
-                  Continue with Github
-                </Button>
 
-                <p className="text-sm font-normal text-neutral-500 dark:text-neutral-400">
-                  Already have an account ?{" "}
-                  <Link
-                    href="/signin"
-                    className="font-medium text-black hover:underline dark:text-teal-500"
-                  >
-                    Sign In
-                  </Link>
-                </p>
-              </Stack>
-            </Form>
-          </Formik>
-        </Box>
-      </Stack>
-    </Flex>
+                  <p className="text-sm font-normal text-neutral-500 dark:text-neutral-400">
+                    Already have an account ?{" "}
+                    <Link
+                      href="/signin"
+                      className="font-medium text-black hover:underline dark:text-teal-500"
+                    >
+                      Sign In
+                    </Link>
+                  </p>
+                </Stack>
+              </Form>
+            </Formik>
+          </Box>
+        </Stack>
+      </Flex>
+    </DashboardLayout>
   );
 }
 
